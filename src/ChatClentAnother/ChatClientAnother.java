18019f6ed4dce80671d9ca017c72cd83a63fba0b
Eugene Berezin <esberezin@gmail.com>
2024-11-28 15:35:21 +0500

    Домашнее задание по Уроку 5. Клиент/Сервер своими руками

---
+++ src/ChatClentAnother/ChatClientAnother.java
@@ -0,0 +1,52 @@
+package ChatClentAnother;
+
+import java.io.*;
+import java.net.*;
+
+public class ChatClientAnother {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader consoleInput;
+
+    public ChatClientAnother(String serverAddress, int serverPort) throws IOException {
+        socket = new Socket(serverAddress, serverPort);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        consoleInput = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void start() {
+        new Thread(new ServerListener()).start();
+        try {
+            String input;
+            while ((input = consoleInput.readLine()) != null) {
+                out.println(input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ServerListener implements Runnable {
+        public void run() {
+            try {
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("Server: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ChatClientAnother client = new ChatClientAnother("localhost", 12345);
+            client.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
